8dfb268b19bdf6933111957afd161077d8f3cb8e
Mian1930 <mianazan3@gmail.com>
2022-10-03 11:27:42 +0500

    Vuex Complete

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,40 @@
 import { createStore } from 'vuex'
-
+import axios from 'axios'
 export default createStore({
   state: {
+    counter:0,
+    colorCode:'yellow'
   },
   mutations: {
+    increaseCounter(state, randomNumber){
+      state.counter+=randomNumber
+    },
+    decreaseCounter(state,randomNumber){
+      state.counter-=randomNumber
+    },
+    setColorCode(state,newValue){
+    state.colorCode=newValue
+    }
   },
   actions: {
+    increaseCounter({commit}){
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response=>{
+        commit('increaseCounter',response.data)
+      })
+    },
+    decreaseCounter({commit}){
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response=>{
+        commit('decreaseCounter',response.data)
+      })
+    },
+    
+  },
+  getters:{
+    counterSquared(state){
+      return state.counter *state.counter
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class="counter"
+   :style="{color:$store.state.colorCode}"
+   >
+    {{$store.state.counter}}
+   </div>
+   <div class="counterSquared">
+    <sup>2</sup>=
+    {{$store.getters.counterSquared}}
+   </div>
+    <div class="buttons">
+       <button @click="$store.dispatch('increaseCounter')">+</button>
+   <button @click="$store.dispatch('decreaseCounter')">-</button> 
+   <input type="text" placeholder="enter color" v-model="colorCode">
+    <!-- using commit method -->
+    <!-- <button @click="$store.commit('increaseCounter')">+</button>
+   <button @click="$store.commit('decreaseCounter')">-</button> -->
+   
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  computed:{
+    colorCode:{
+    get(){
+    return this.$store.state.colorCode
+    },
+    set(newValue){
+    this.$store.commit('setColorCode',newValue)
+    }
+  }
   }
 }
 </script>
+<style>
+
+.buttons button{
+  margin-bottom: 10px;
+  font-size: 40px;
+  margin: 0px 10px;
+}
+</style>
